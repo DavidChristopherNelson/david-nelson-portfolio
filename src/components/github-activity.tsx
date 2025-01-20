9a13f1ba338350e622e8cf8bf38df17c55b10022
David Nelson <david.nelson1@uqconnect.edu.au>
2025-01-20 10:25:36 +1000

    Add link to github and start experimenting with fading backgrounds.

--- src/components/github-activity.tsx
+++ src/components/github-activity.tsx
@@ -3,7 +3,14 @@ import GitHubCalendar from 'react-github-calendar';
 function GitHubActivity() {
   return (
     <div className="my-4">
-      <h2 className="text-xl font-bold mb-2">My GitHub Activity</h2>
+      <a 
+        href="https://github.com/DavidChristopherNelson" 
+        target="_blank" 
+        rel="noopener noreferrer" 
+        className="text-xl font-bold mb-2"
+      >
+        <h2>My GitHub Activity</h2>
+      </a>
       <GitHubCalendar
         username="DavidChristopherNelson"
         blockSize={15}
