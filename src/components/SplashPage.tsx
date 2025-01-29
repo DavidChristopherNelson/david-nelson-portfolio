136620dfe0b8fc31d73fd04971941789893ea547
David Nelson <david.nelson1@uqconnect.edu.au>
2025-01-29 15:41:19 +1000

    Refactored code

--- src/components/SplashPage.tsx
+++ src/components/SplashPage.tsx
@@ -13,93 +13,91 @@ const SplashPage = () => {
   }, []);
 
   return(
-    <>
-      <div 
+    <div 
+      className="
+          w-screen 
+          h-screen 
+          relative
+          flex 
+          flex-row
+          items-center
+          justify-between
+          bg-black"
+    >
+      <div
         className="
-            w-screen 
-            h-screen 
-            relative
-            flex 
-            flex-row
-            items-center
-            justify-between
-            bg-black"
+          relative
+          w-full
+          h-full
+          flex
+          items-center
+        "
       >
         <div
           className="
+            p-20
             relative
-            w-full
-            h-full
-            flex
-            items-center
+            z-10
           "
         >
-          <div
+          <h1 
             className="
-              p-20
-              relative
-              z-10
+              text-[12rem] 
+              font-mono 
+              -translate-x-3
+              leading-[0.9]
             "
           >
-            <h1 
-              className="
-                text-[12rem] 
-                font-mono 
-                -translate-x-3
-                leading-[0.9]
-              "
-            >
-              David
-            </h1>
-            <h1 
-              className="
-                text-[12rem] 
-                font-mono 
-                leading-[0.9]
-              "
-            >
-              Nelson
-            </h1>
-            <p
-              className="text-2xl"
-            >
-              <br />
-              US and Australian Citizen<br />
-              <br />
-              
-              Previous Software Developer at Dubber and DecoNetwork.<br />
-              Degrees in Chemical Engineering and Physics.<br />
-              Raised over $100,000 in funding for my startups.<br />
-              <br />
-              hello@davidnelson.bio<br />
-            </p>
-          </div>
-          {Array(10).fill(0).map((_, i) => (
-            <FadingBackground key={i} />
-          ))}
+            David
+          </h1>
+          <h1 
+            className="
+              text-[12rem] 
+              font-mono 
+              leading-[0.9]
+            "
+          >
+            Nelson
+          </h1>
+          <p
+            className="text-2xl"
+          >
+            <br />
+            US and Australian Citizen<br />
+            <br />
+            
+            Previous Software Developer at Dubber and DecoNetwork.<br />
+            Degrees in Chemical Engineering and Physics.<br />
+            Raised over $100,000 in funding for my startups.<br />
+            <br />
+            hello@davidnelson.bio<br />
+          </p>
         </div>
-        <div className="relative">
-          <img 
-            ref={imgRef}
-            src={profilePicture}
-            alt="Example Image" 
-            className="my-container w-auto h-screen [max-width:none]"
-          />
-          {Array(10).fill(0).map((_, i) => (          
-            <FadingBackground
-              key={i}
-              className=" "
-              style={{
-                top: `${Math.floor(0.20 * imgWidth)}px`,
-                right: `${Math.floor(0.33 * imgWidth)}px`,
-                width: `${Math.floor(0.2 * imgWidth)}px`,
-                height: `${Math.floor(0.5 * imgWidth)}px`,
-              }}
-            />
-          ))}
-        </div>
+        {Array(10).fill(0).map((_, i) => (
+          <FadingBackground key={i} />
+        ))}
       </div>
-    </>
+      <div className="relative">
+        <img 
+          ref={imgRef}
+          src={profilePicture}
+          alt="Example Image" 
+          className="my-container w-auto h-screen [max-width:none]"
+        />
+        {Array(10).fill(0).map((_, i) => (          
+          <FadingBackground
+            key={i}
+            className=" "
+            style={{
+              top: `${Math.floor(0.20 * imgWidth)}px`,
+              right: `${Math.floor(0.33 * imgWidth)}px`,
+              width: `${Math.floor(0.2 * imgWidth)}px`,
+              height: `${Math.floor(0.5 * imgWidth)}px`,
+            }}
+          />
+        ))}
+      </div>
+    </div>
   );
 };
 
