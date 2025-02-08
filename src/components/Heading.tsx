6ba910006bedae87cb4cf5bcb22bae3418e14d84
David Nelson <david.nelson1@uqconnect.edu.au>
2025-02-08 11:01:38 +1000

    Refactor code.

--- src/components/Heading.tsx
+++ src/components/Heading.tsx
@@ -1,7 +1,11 @@
 const Heading = ({text}: {text: string}) => {
   return (
     <div
-        className="text-8xl text-[#008080] my-10"
+        className="
+          text-8xl 
+          text-[#008080] 
+          my-10
+        "
       >
         {text}
     </div>
