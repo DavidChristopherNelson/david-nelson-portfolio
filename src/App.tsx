1e2e47c601abbc617e7439b75b2ded069b6b4dba
DavidChristopherNelson <david.nelson1@uqconnect.edu.au>
2025-09-11 13:31:46 +0530

    Update content.

--- src/App.tsx
+++ src/App.tsx
@@ -56,8 +56,8 @@ const App = () => {
     { name: "Convex", frequency: 1 },
     { name: "S3", frequency: 1 },
     { name: "Clerk", frequency: 1 },
-    { name: "Shadcn", frequency: 1 },
     { name: "NextAuth", frequency: 1 },
+    { name: "Shadcn", frequency: 1 },
     { name: "Applescript", frequency: 1 }
   ];
 
